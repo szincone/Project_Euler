0511ba413b7062506c3d784bcf438427b0abe7cb
szincone <sawyerzincone@gmail.com>
2018-04-29 00:43:33 -0600

    Update prob_1.py

--- 1to100/prob_1.py
+++ 1to100/prob_1.py
@@ -1,5 +1,5 @@
 def mults(x, y):
-    ### prints the sum of all the multiples of x and y
+    """ prints the sum of all the multiples of x and y """
     sum = 0
     for i in range(1, 1000):
         if (i % x) == 0 or (i % y) == 0:
