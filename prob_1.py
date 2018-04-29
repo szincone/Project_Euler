bf4aaa84b62e049b458593718ced43b054fcc4aa
szincone <sawyerzincone@gmail.com>
2018-04-29 00:23:11 -0600

    Create prob_1.py

---
+++ prob_1.py
@@ -0,0 +1,11 @@
+def mults(x, y):
+    ### prints the sum of all the multiples of x and y
+    sum = 0
+    for i in range(1, 1000):
+        if (i % x) == 0 or (i % y) == 0:
+            sum += i
+    
+    print(sum)
+        
+
+mults(3,5)
